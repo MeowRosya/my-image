2c1d2d81b9b8c2273a85d7ee788b040ca6c541f6
Elizarov Yaroslav <132341650+MeowRosya@users.noreply.github.com>
2024-12-02 18:57:27 +0000

    a

--- build.sh
+++ build.sh
@@ -13,7 +13,12 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+
+rpm-ostree install hyprland waybar nautilus wofi kitty
+rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda
+rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1
+rpm-ostree install tuned tuned-ppd
+rpm-ostree install distrobox flatpak network-manager-applet pipewire wireplumber pipewire-pulse pipewire-alsa
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
